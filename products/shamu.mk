b0dc0ddde794672f0725ae64388003808acee847
Richard Covington <rcovington79@gmail.com>
2017-07-28 21:32:39 -0400

    Update shamu.mk

--- products/shamu.mk
+++ products/shamu.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 # Include pure telephony configuration
-include vendor/aosp/configs/pure_phone.mk
+include vendor/aosp/configs/aosp_phone.mk
 
 # Inherit AOSP device configuration for shamu.
 $(call inherit-product, device/moto/shamu/aosp_shamu.mk)
