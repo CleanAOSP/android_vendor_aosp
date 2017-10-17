514b69d862194461dc792e634fc2dc608fb4afb4
jb <jb@bbox>
2017-10-16 21:25:51 -0400

    Remove unused devices

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,2 @@
-add_lunch_combo angler-user
-add_lunch_combo bullhead-user
-add_lunch_combo dragon-user
-add_lunch_combo marlin-user
-add_lunch_combo sailfish-user
+add_lunch_combo grouper-user
 add_lunch_combo shamu-user
