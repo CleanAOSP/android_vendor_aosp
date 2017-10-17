9a443a46b54b67cb94d102c221f8f781e8b81583
Jory <jor.bunch@gmail.com>
2017-10-17 01:08:57 -0400

    Update release keys

--- products/shamu.mk
+++ products/shamu.mk
@@ -28,5 +28,8 @@ PRODUCT_MANUFACTURER := motorola
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=shamu \
-    BUILD_FINGERPRINT=google/shamu/shamu:7.1.1/NGI55D/4298276:user/release-keys \
-    PRIVATE_BUILD_DESC="shamu-user 7.1.1 NGI55D 4298276 release-keys"
+# Device Fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=shamu \
+    BUILD_FINGERPRINT=google/shamu/shamu:7.1.2/NZH54D/4298276:user/release-keys \
+    PRIVATE_BUILD_DESC="shamu-user 7.1.2 NZH54D 4298276 release-keys"
