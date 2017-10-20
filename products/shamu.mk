5c38ab7dd4a3ce9fa0d6a1a63d6216559eb6e491
Jory <jor.bunch@gmail.com>
2017-10-20 15:46:25 -0400

    fix fingerprint

--- products/shamu.mk
+++ products/shamu.mk
@@ -31,5 +31,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=shamu \
-    BUILD_FINGERPRINT=google/shamu/shamu:7.1.2/NZH54D/4298276:user/release-keys \
-    PRIVATE_BUILD_DESC="shamu-user 7.1.2 NZH54D 4298276 release-keys"
+    BUILD_FINGERPRINT=google/shamu/shamu:7.1.1/N8I11B/4171878:user/release-keys \
+    PRIVATE_BUILD_DESC="shamu-user 7.1.1 N8I11B 4171878 release-keys"
